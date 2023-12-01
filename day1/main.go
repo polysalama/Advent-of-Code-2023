111de6ba27c0139f2b8e04786b7d1658f7a68b1a
Ziga Resnik <zigaresnik@gmail.com>
2023-12-01 13:34:12 +0100

    day 1

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/polysalama/aoc_2023/utils"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var lines []string = utils.ReadToSlice("./day1/input.txt")
+
+	var sumPart1 int = 0
+	var sumPart2 int = 0
+
+	numbers := []string{
+		"1", "2", "3", "4", "5", "6", "7", "8", "9",
+	}
+
+	numbersAsWords := []string{
+		"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+	}
+
+	for _, line := range lines {
+
+		partOneNumbers := utils.FindAllNumbersInString(line, numbers)
+		partTwoNumbers := utils.FindAllNumbersInString(line, append(append([]string{}, numbers...), numbersAsWords...))
+
+		n1, err1 := strconv.Atoi(
+			utils.StringToNumberString(partOneNumbers[0]) + utils.StringToNumberString(partOneNumbers[len(partOneNumbers)-1]),
+		)
+		if err1 != nil {
+			fmt.Println(err1)
+		}
+		sumPart1 += n1
+
+		n2, err2 := strconv.Atoi(
+			utils.StringToNumberString(partTwoNumbers[0]) + utils.StringToNumberString(partTwoNumbers[len(partTwoNumbers)-1]),
+		)
+
+		if err2 != nil {
+			fmt.Println(err2)
+		}
+		sumPart2 += n2
+	}
+
+	fmt.Println(sumPart1)
+	fmt.Println(sumPart2)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,12 @@
 package utils
 
 import (
-	"os"
 	"bufio"
 	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func ReadToSlice(path string) []string {
@@ -25,4 +28,52 @@ func ReadToSlice(path string) []string {
 	}
 
 	return lines
+}
+
+func StringToNumberString(s string) string {
+	stringToNumberString := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return stringToNumberString[s]
+	}
+
+	return strconv.Itoa(i)
+}
+
+func MapValuesToOrderedSlice(m map[int]string) []string {
+	var keys []int
+	var values []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		values = append(values, m[k])
+	}
+	return values
+}
+
+func FindAllNumbersInString(s string, numbers []string) []string {
+
+	var foundStringsMap map[int]string = make(map[int]string)
+
+	for _, number := range numbers {
+		if strings.Contains(s, number) {
+			foundStringsMap[strings.LastIndex(s, number)] = number
+			foundStringsMap[strings.Index(s, number)] = number
+		}
+	}
+
+	return MapValuesToOrderedSlice(foundStringsMap)
 }
